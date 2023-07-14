c0dac75ba81ca9dc95c3bcf35bdf2fcf0b3229c2
Jorg Jenni <jorg.jenni@jennius.co.uk>
2023-07-14 14:44:41 +0100

    modularize app

--- app/routes.rb
+++ app/routes.rb
@@ -2,12 +2,12 @@
 
 class Routes
   PATHS = {
-    'portfolio/show' => ->(user_id:) { PortfolioController.new.show(user_id:) },
-    'investment_fees/show' => ->(user_id:) { InvestmentFeesController.new.show(user_id:) },
-    'investment_news/show' => ->(user_id:) { InvestmentNewsController.new.show(user_id:) },
-    'login/show' => -> { LoginController.new.show },
-    'admin/show' => ->(user_id:) { AdminController.new.show(user_id:) },
-    'accounting/show' => -> { AccountingController.new.show },
-    'profile/show' => ->(user_id:) { ProfileController.new.show(user_id:) }
+    'portfolio/show' => ->(user_id:) { Securities::PortfolioController.new.show(user_id:) },
+    'investment_fees/show' => ->(user_id:) { Securities::InvestmentFeesController.new.show(user_id:) },
+    'investment_news/show' => ->(user_id:) { Securities::InvestmentNewsController.new.show(user_id:) },
+    'login/show' => -> { Admin::LoginController.new.show },
+    'admin/show' => ->(user_id:) { Admin::AdminController.new.show(user_id:) },
+    'accounting/show' => -> { Accounting::AccountingController.new.show },
+    'profile/show' => ->(user_id:) { Admin::ProfileController.new.show(user_id:) }
   }.freeze
 end
